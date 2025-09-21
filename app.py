78a96754ed51d940921a6534eb7f42cdb4bbb0e8
H3KLER <157891859+h3kler07@users.noreply.github.com>
2025-09-21 09:44:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,4 +63,5 @@ def verify_password():
         return jsonify({"status": "error", "message": "Incorrect password"}), 401
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host = '0.0.0.0',debug=True)
+
